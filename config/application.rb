3e4cbfd9daa8deddffdfc063464d3ee1eab01cb4
Roelly Wang <a123458524@yahoo.com.tw>
2026-04-12 09:26:47 +0800

    adjust_ci

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module FlightTicketPrice
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locales = [:"zh-TW", :en]
+    config.i18n.available_locales = [ :"zh-TW", :en ]
     config.i18n.default_locale = :"zh-TW"
     config.i18n.fallbacks = true
   end
